808cb6a35bcc7cd62567d2119936aa1312a9e29f
Gazel <gazel@ngoprekweb.com>
2024-10-20 18:46:30 +0700

    fix: case sensitive name in create anggota view

--- resources/views/v_anggota/create.blade.php
+++ resources/views/v_anggota/create.blade.php
@@ -2,7 +2,7 @@
 <form action="{{route('anggota.store')}}" method="post">
     @csrf
     <label>Nama</label><br>
-    <input type="text" name="Nama" id="" value="{{old('nama')}}" placeholder="Masukan Nama Lengkap" class="form-control @error('nama') is-invalid @enderror">
+    <input type="text" name="nama" id="" value="{{old('nama')}}" placeholder="Masukan Nama Lengkap" class="form-control @error('nama') is-invalid @enderror">
     @error('nama')
     <span class="invalid-feedback alert-danger" role="alert">
         {{$message}}
@@ -10,7 +10,7 @@
     @enderror
     <p></p>
     <label for="">hp</label><br>
-    <input type="text" name="Hp" id="" value="{{old('hp')}}" placeholder="Masukan Nomor Hp"
+    <input type="text" name="hp" id="" value="{{old('hp')}}" placeholder="Masukan Nomor Hp"
     class="form-control @error('hp') is-invalid @enderror">
     @error('hp')
     <span class="invalid-feedback alert-danger" role="alert">
